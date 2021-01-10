0339ba2b615dfce1c3e6a458b83f83f844ee5f4b
Bach Le <bachcyp@gmail.com>
2021-01-09 19:11:50 -0800

    added twitter embed

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { TwitterTweetEmbed } from 'react-twitter-embed';
+import './css/App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { apiResponse: ""};
+	}
+
+	callAPI() {
+		fetch("http://localhost:8000/testAPI")
+			.then(res => res.text())
+			.then(res => this.setState({ apiResponse: res}))
+			.catch(err => err);
+	}
+
+	componentDidMount() {
+		this.callAPI();
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<p className="App-intro">{this.state.apiResponse}</p>
+					<TwitterTweetEmbed tweetId={'933354946111705097'}/>
+					{this.children}
+				</header>
+			</div>
+		);
+	}
 }
 
 export default App;
