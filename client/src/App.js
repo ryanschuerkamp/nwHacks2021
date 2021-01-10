0261470fae3d87c828ee50e94aa721154cb57364
Kat Sukkau <Katsukkau@gmail.com>
2021-01-10 10:09:41 -0800

    added links for accurate information

--- client/src/App.js
+++ client/src/App.js
@@ -36,6 +36,15 @@ class App extends Component {
 				<div className="App-flex-container">
 					<img src="" id="wordCloud" alt=""></img>
 				</div>
+				<div>
+				<h5 className="App-heading">INFO</h5>
+				<div className="App-flex-container">
+				For current scientific information regarding 
+				the vaccines please select you country below.
+				<a href="https://www.canada.ca/en/public-health/services/diseases/coronavirus-disease-covid-19/vaccines.html?&utm_campaign=hc-sc-covidvaccine&utm_medium=sem&utm_source=ggl&utm_content=ad-text-en&utm_term=covid%2019%20vaccines&adv=2021-0051&id_campaign=11503036027&id_source=113997067404&id_content=487727621650">CANADA</a>
+				<a href="https://www.cdc.gov/coronavirus/2019-ncov/vaccines/8-things.html#:~:text=COVID%2D19%20vaccination%20will%20help,offer%20against%20this%20serious%20disease.">USA</a>
+				</div>
+				</div>
 			</div>
 		);
 	}
